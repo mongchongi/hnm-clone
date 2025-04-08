792f441f5eccafc78e6ce4787ae86345c2669eee
mongchongi <yeonguk0517@gmail.com>
2025-04-08 14:14:00 +0900

    데이터 가져오기

--- src/pages/ProductAll.jsx
+++ src/pages/ProductAll.jsx
@@ -1,4 +1,22 @@
+import { useEffect, useState } from 'react';
+
 const ProductAll = () => {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    try {
+      const res = await fetch('http://localhost:4000/products');
+      const data = await res.json();
+      setProducts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return <div>ProductAll</div>;
 };
 
