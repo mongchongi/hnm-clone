5e17024e4d861b4f2fe8c54b06cc81810e4b61b3
mongchongi <yeonguk0517@gmail.com>
2025-04-08 23:18:09 +0900

    스크롤 이동 버튼 구현

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,26 @@ import './App.css';
 import ProductAll from './pages/ProductAll/ProductAll';
 import SignIn from './pages/SignIn/SignIn';
 import Navbar from './components/Navbar/Navbar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PrivateRoute from './route/PrivateRoute';
+import { faUpLong } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const App = () => {
   const [authenticate, setAuthenticate] = useState(false); // true => 로그인 O, false => 로그인 X
+  const [showTop, setShowTop] = useState(false);
+
+  const toggleTopButton = () => {
+    if (window.scrollY > 100) {
+      setShowTop(true);
+    } else {
+      setShowTop(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleTopButton);
+  }, []);
 
   return (
     <div>
@@ -17,6 +32,11 @@ const App = () => {
         <Route path='/product/:id' element={<PrivateRoute authenticate={authenticate} />} />
         <Route path='/sign-in' element={<SignIn setAuthenticate={setAuthenticate} />} />
       </Routes>
+      {showTop && (
+        <button className='top-button' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+          <FontAwesomeIcon icon={faUpLong} className='top-button__icon' />
+        </button>
+      )}
     </div>
   );
 };
